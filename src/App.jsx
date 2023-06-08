666348315b72205ce2a2c37d5ebb746ff9d1397e
ishak-dev <ishak.kazic@gmail.com>
2023-06-09 00:19:07 +0200

    add token decoder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Routes, Route, Link, Router } from "react-router-dom";
+import { Routes, Route, Link, Router, Navigate } from "react-router-dom";
 import Navbar from "./components/navbar";
 import UserDashboard from "./pages/UserDashboard";
 import Home from "./pages/home";
@@ -16,14 +16,20 @@ import AdminNavbar from "./components/adminNavbar";
 import CarServiceHistory from "./pages/carServiceHistory";
 import AdminDashboard from "./pages/adminDashboard";
 import AdminArticles from "./pages/adminArticles";
+import { useJwt } from "react-jwt";
 
 function App() {
-  const [user, setUser] = useState("admin");
-
+  const [user, setUser] = useState("users");
+  const token = localStorage.getItem("token");
+  const { decodedToken, isExpired } = useJwt(token);
+  console.log(decodedToken);
   return (
     <div className="App">
-      {user == "admin" ? <AdminNavbar /> : <Navbar />}
-
+      {decodedToken != undefined && decodedToken.role == "admin" ? (
+        <AdminNavbar />
+      ) : (
+        <Navbar />
+      )}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Home />} />
@@ -31,8 +37,38 @@ function App() {
         <Route path="/schedule" element={<Schedule />} />
         <Route path="/orders" element={<Orders user={user} />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route
+          path="/login"
+          element={
+            decodedToken ? (
+              <Navigate
+                to={
+                  decodedToken.role == "admin"
+                    ? "/adminDashboard"
+                    : "/dashboard"
+                }
+              />
+            ) : (
+              <Login />
+            )
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            decodedToken ? (
+              <Navigate
+                to={
+                  decodedToken.role == "admin"
+                    ? "/adminDashboard"
+                    : "/dashboard"
+                }
+              />
+            ) : (
+              <Register />
+            )
+          }
+        />
         <Route path="/carshistory" element={<CarServiceHistory />} />
         <Route path="/adminArticles" element={<AdminArticles />} />
         <Route path="/adminDashboard" element={<AdminDashboard />} />
