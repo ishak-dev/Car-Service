31713ae48032af6cfff30469d3dc3652d012307d
ishak-dev <ishak.kazic@gmail.com>
2023-06-12 23:48:13 +0200

    Update schedule.jsx

--- src/pages/schedule.jsx
+++ src/pages/schedule.jsx
@@ -1,62 +1,79 @@
 import React, { useEffect, useState } from "react";
 import "../style/pages/schedule.css";
 import ScheduleCard from "../components/schedule/scheduleCard";
-import { allUserServices } from "../api/api";
+import { allUserServices, getVehicleById } from "../api/api";
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import ScheduleModal from "../components/schedule/scheduleModal";
+import CarModal from "../components/schedule/carModal";
 const Schedule = () => {
   const [counter, setCounter] = useState(0);
   const [schedules, setSchedules] = useState([]);
   const [show, setShow] = useState(false);
+  const [show2, setShow2] = useState(false);
+  const [vehicle, setVehicle] = useState("");
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const handleClose2 = () => setShow2(false);
+  const handleShow2 = () => setShow2(true);
   useEffect(() => {
     allUserServices().then((data) => setSchedules(data));
+    getVehicleById().then((data) => setVehicle(data));
   }, []);
+
   useEffect(() => {
-    counter > 0 && toast.success("You scheduled an appointment");
+    counter > 0 && toast.success("Successfull");
   }, [counter]);
+
   return (
     <div className="schedule-section">
       <h2>
-        Your Appointment
-        <button className="create-new-appointment-btn" onClick={handleShow}>
-          Create New
-        </button>
+        Your Appointment (Please add car first if you didn't)
+        {!vehicle ? (
+          <button className="create-new-appointment-btn" onClick={handleShow2}>
+            Add Vehicle
+          </button>
+        ) : (
+          <button className="create-new-appointment-btn" onClick={handleShow}>
+            Create New
+          </button>
+        )}
       </h2>
       <div className="card-colors">
-        <li>Big Service</li>
-        <li>Medium Service</li>
-        <li>Small Service</li>
+        {vehicle != "" && (
+          <>
+            <li>{vehicle.Make}</li>
+            <li>{vehicle.Model}</li>
+            <li>{vehicle.LicensePlate}</li>
+          </>
+        )}
       </div>
       <div className="appointment-cards">
-        {schedules.length > 1
-          ? schedules.map((schedule) => (
-              <ScheduleCard
-                key={schedule.id}
-                date={schedule.ServiceDate}
-                vehicleId={schedule.VehicleId}
-                serviceTypeId={schedule.serviceTypeId}
-                description={schedule.description}
-                hours={schedule.hours}
-              />
-            ))
-          : schedules != [] && (
-              <ScheduleCard
-                date={schedules.ServideDate}
-                vehicleId={schedules.VehicleId}
-                serviceTypeId={schedules.serviceTypeId}
-                description={schedules.description}
-                hours={schedules.hours}
-              />
-            )}
+        {schedules.length != [] ? (
+          schedules.map((schedule) => (
+            <ScheduleCard
+              key={schedule.id}
+              date={schedule.ServiceDate}
+              vehicleId={schedule.VehicleId}
+              serviceTypeId={schedule.serviceTypeId}
+              description={schedule.description}
+              hours={schedule.hours}
+            />
+          ))
+        ) : (
+          <p>You dont have appointments</p>
+        )}
       </div>
       <ScheduleModal
         handleClose={handleClose}
         show={show}
+        setCounter={setCounter}
+      />
+      <CarModal
+        handleClose={handleClose2}
+        show={show2}
         setCounter={setCounter}
       />
       <ToastContainer theme="dark" autoClose={2000} />
